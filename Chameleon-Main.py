eadc992082da17bab06f8225fe761ecebe54fe2e
JacobKay97 <Jacob.kay97@gmail.com>
2018-01-28 12:20:29 +0000

    Add files via upload

---
+++ Chameleon-Main.py
@@ -0,0 +1,72 @@
+########### Python 3.2 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64, sys
+import simplejson as json
+import numpy as np
+import cv2
+import time
+import serial
+
+headers = {
+    # Request headers. Replace the placeholder key below with your subscription key.
+    'Content-Type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': 'd5a5e3e7f3c248bc999729b5b483fec0',
+}
+
+params = urllib.parse.urlencode({
+})
+
+req = ['anger','contempt','disgust','fear','happiness','neutral','sadness','surprise']
+
+
+ser = serial.Serial('COM8',9600)
+
+im = cv2.VideoCapture(0)
+while(1):
+
+    ret, frame = im.read()
+    cv2.imshow('frame',frame)
+
+    #cv2.waitKey(0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    img_str = cv2.imencode('.jpg', frame)[1].tostring()
+    body = bytearray(img_str)
+
+    try:
+        # NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+        #   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the
+        #   URL below with "westcentralus".
+        conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
+        response = conn.getresponse()
+        data = response.read()
+        # 'data' contains the JSON data. The following formats the JSON data for display.
+        parsed = json.loads(data)
+        #print ("Response:")
+        #print (json.dumps(parsed, sort_keys=True, indent=2))
+        conn.close()
+    except Exception as e:
+        print(e.args)
+    ####################################
+    if parsed:
+        moodvals = []
+        for x in req:
+            moodvals.append(parsed[0]['scores'][x])
+
+        print(moodvals)
+        maxmood = max(moodvals);
+        print(maxmood)
+        maxmoodindex = [i for i, j in enumerate(moodvals) if j == maxmood]
+        print (maxmoodindex)
+        maxmood =int(maxmood*100)
+        print(maxmood)
+        print(req[maxmoodindex[0]])
+        towrite = str(maxmoodindex[0]) + ',' + str(maxmood)
+        #ser.write(bytearray(maxmoodindex,maxmood))
+        ser.write(bytes(towrite,'ASCII'))
+        #ser.write(bytes(maxmood))
+    time.sleep(0.2)
+
+im.release()
+cv2.destroyAllWindows()
+ser.close()
---
+++ LEDTEST/LEDTEST.ino
@@ -0,0 +1,47 @@
+#include <Adafruit_NeoPixel.h>
+#define PIN 6
+
+int colours[8][3]={{204, 0, 0},{191,0,151},{171, 9, 36},{0,204,0},{150,125,0},{200,200,200},{25,25,240},{0, 125, 53}};
+
+/*anger: red
+"contempt": reddy purple
+"disgust": pink
+"fear": green
+"happiness": light yellow,
+"neutral": white
+"sadness": blue
+"surprise: teal*/
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  strip.begin();
+  strip.clear();
+  strip.show();
+  Serial.begin(9600);
+  Serial.print("Ready");
+}
+
+void loop() {
+  if (Serial.available() >= 4) {
+    int colour = Serial.parseInt();
+    int strength = Serial.parseInt();
+    Serial.print("Rx");
+    Serial.print(colour);
+    Serial.print(":");
+    Serial.print(strength);
+    Serial.print('\n');
+
+    int r = strength*colours[colour][0] /100;
+    int g = strength*colours[colour][1] /100;
+    int b = strength*colours[colour][2] /100;
+
+    for (int i=0; i <= 86; i++){
+      strip.setPixelColor(i,r,g,b);
+    }
+    int brightness = (200*strength)/brightness;
+    strip.setBrightness(brightness);
+    strip.show();
+  }
+}
